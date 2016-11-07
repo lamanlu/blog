6f0dc31a119bdcd0eab56db54abb4d3f20008689
lamanlu <bonacc@126.com>
2016-11-07 18:11:04 +0800

    jpa

---
+++ src/main/java/com/blog/Entity/Article.java
@@ -0,0 +1,7 @@
+package com.blog.Entity;
+
+/**
+ * Created by LamanLu on 2016/11/7.
+ */
+public class Article {
+}
---
+++ src/main/java/com/blog/entity/Category.java
@@ -0,0 +1,7 @@
+package com.blog.entity;
+
+/**
+ * Created by LamanLu on 2016/11/7.
+ */
+public class Category {
+}
